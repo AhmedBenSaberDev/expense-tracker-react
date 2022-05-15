5093aaaecbdbce805d8c6bed64e293d1b2a03859
AhmedBenSaberDev <ahmedbensaber.dev@gmail.com>
2022-05-15 16:41:53 +0200

    format

--- src/main/List/List.jsx
+++ src/main/List/List.jsx
@@ -24,7 +24,7 @@ const List = () => {
                             <MoneyOff/>
                         </Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary={transaction.category} secondary={`${transaction.amount} - ${transaction.date}`}/>
+                    <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`}/>
                     <ListItemSecondaryAction>
                         <IconButton edge='end' aria-label="delete" onClick={() => {deleteTransaction(transaction.id)}}>
                             <Delete/>
